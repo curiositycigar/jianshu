831ad8bee262a3ec455eed1b04dc7b1bfa7d89c4
YOU <youwg@suninfo.com>
2018-03-20 17:46:05 +0800

    数据库连接

--- app/models/index.js
+++ app/models/index.js
@@ -1,67 +1,37 @@
 /**
  * Created by YOU on 2018/3/19.
+ * document http://docs.sequelizejs.com/manual/tutorial/models-definition.html
  */
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('test', 'test', '123456', {
-    host: '192.168.175.130',
-    port: 3306,
-    dialect: 'mysql',
+const sequelize = new Sequelize('test', 'mysql', '123456', {
+  host: '192.168.212.41',
+  port: 3306,
+  dialect: 'mysql',
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    operatorsAliases: false
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
 });
 
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
-const Author = sequelize.define('author', {
-    author_id: {
-        type: Sequelize.BIGINT,
-        autoIncrement: true,
-        primaryKey: true,
-        unique: true
-    },
-    avator_id: {
-        type: Sequelize.CHAR(30)
-    },
-    nick_name: {
-        type: Sequelize.CHAR(30)
-    },
-    sex: {
-        type: Sequelize.TINYINT
-    },
-    introduction: {
-        type: Sequelize.STRING(300)
-    },
-    website: {
-        type: Sequelize.CHAR(100)
-    },
-    er_code: {
-        type: Sequelize.BIGINT
-    },
-    createtime: {
-        type: Sequelize.DATE
-    },
-    reword_open: {
-        type: Sequelize.BLOB
-    },
-    rewoed_description: {
-        type: Sequelize.STRING(300)
-    }
-})
+const Author = require('./author')(Sequelize, sequelize)
 
 Author.sync().then(() => {
-    console.log('success!!')
+  return Author.create({
+    password: '12321321',
+    email: 'aasdsad@weq.com'
+  });
 })
