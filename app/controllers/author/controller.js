b2d99c41cbccf6223bf84d6c377b205fd9e0f51f
YOU <youwg@suninfo.com>
2018-03-21 18:07:32 +0800

    author Controller

--- app/controllers/author/controller.js
+++ app/controllers/author/controller.js
@@ -2,6 +2,16 @@
  * Created by YOU on 2018/3/19.
  */
 
+const Author = require('../../models').Author
+
+Author.sync().then(function () {
+  return Author.create({
+    email: 'asda@163.com',
+    password: '123213',
+    sex: 0
+  })
+})
+
 exports.getUserbaseInfo = async (ctx, next) => {
   ctx.body = {
     name: 'username'
