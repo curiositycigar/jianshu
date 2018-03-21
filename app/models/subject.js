59292aaeeb183c474d80274c4db3d08a328e72d3
YOU <youwg@suninfo.com>
2018-03-21 09:44:37 +0800

    Model 定义

---
+++ app/models/subject.js
@@ -0,0 +1,53 @@
+/**
+ * Created by YOU on 2018/3/21.
+ */
+module.exports = function (Sequelize, sequelize) {
+  return sequelize.define('author', {
+    subject_id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV1
+    },
+    name: {
+      type: Sequelize.CHAR(50),
+      allowNull: false
+    },
+    author_id: {
+      type: Sequelize.UUID,
+      unique: true,
+      allowNull: false
+    },
+    cover_id: {
+      type: Sequelize.UUID
+    },
+    description: {
+      type: Sequelize.STRING(300),
+      allowNull: false
+    },
+    article_count: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    follow_count: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    create_date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false
+    },
+    allow_contribute: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    },
+    should_audit: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    }
+  })
+}
