cf9040bad2d8592a9042a33fb95569d3f9cd8129
YOU <youwg@suninfo.com>
2018-03-21 10:43:53 +0800

    Model 定义

---
+++ app/models/sys_message.js
@@ -0,0 +1,39 @@
+/**
+ * Created by YOU on 2018/3/21.
+ */
+module.exports = function (Sequelize, sequelize) {
+  return sequelize.define('sys_message', {
+    message_id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV1
+    },
+    author_id: {
+      type: Sequelize.UUID,
+      allowNull: false
+    },
+    message_type: {
+      type: Sequelize.TINYINT,
+      allowNull: false,
+      defaultValue: 0
+    },
+    content: {
+      type: Sequelize.STRING(800),
+      allowNull: false
+    },
+    link: {
+      type: Sequelize.CHAR(100),
+      allowNull: false
+    },
+    is_readed: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    },
+    comment_date: {
+      type: Sequelize.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.NOW
+    }
+  })
+}
