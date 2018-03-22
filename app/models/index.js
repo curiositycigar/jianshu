75f1690daf956fd45b45580d40ec29b022f15e8a
YOU <youwg@suninfo.com>
2018-03-22 10:38:17 +0800

    移动配置到config

--- app/models/index.js
+++ app/models/index.js
@@ -2,21 +2,15 @@
  * Created by YOU on 2018/3/19.
  * document http://docs.sequelizejs.com/manual/tutorial/models-definition.html
  */
+const {
+  user,
+  password,
+  database,
+  options
+} = require('../config').databaseConfig
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('test', 'mysql', '123456', {
-  host: '192.168.212.41',
-  port: 3306,
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  operatorsAliases: false
-});
+const sequelize = new Sequelize(database, user, password, options);
 
 sequelize
   .authenticate()
