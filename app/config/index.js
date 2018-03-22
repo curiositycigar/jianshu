62a071197f022b71d5f3685c5bdb2e6727c66204
YOU <youwg@suninfo.com>
2018-03-22 10:37:52 +0800

    配置文件

--- app/config/index.js
+++ app/config/index.js
@@ -2,5 +2,27 @@
  * Created by YOU on 2018/3/19.
  */
 module.exports = {
-  port: 8234
+  port: 8234,
+  auth: {
+    expiresIn: '1h',
+    secret: 'secret',
+    admin: 100,
+  },
+  databaseConfig: {
+    user: 'mysql',
+    password: '123456',
+    database: 'test',
+    options: {
+      host: '192.168.212.41',
+      port: 3306,
+      dialect: 'mysql',
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      },
+      operatorsAliases: false
+    }
+  }
 }
