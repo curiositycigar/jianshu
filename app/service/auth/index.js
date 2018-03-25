2a995cc44e506b632558784726c8e53abad06a2c
curiositycigar <you11098@163.com>
2018-03-25 23:16:28 +0800

    添加用户、验证用户服务

--- app/service/auth/index.js
+++ app/service/auth/index.js
@@ -2,17 +2,19 @@
  * Created by YOU on 2018/3/22.
  */
 const Author = require('../../models').Author
-const encrypt = require('../../utils')
+const encrypt = require('../../utils').encrypt
 
 const createAuthor = (params) => {
-  return Author.create(params).then(data => data, err => err)
+    return Author.create(params).then(data => data.result, err => err)
 }
 const validateAccount = (params) => {
-  params.password = encrypt(params.password)
-  return Author.findOne().then(data => data, err => err)
+    params.password = encrypt(params.password)
+    return Author.findOne({
+        where: params
+    }).then(data => data, err => err)
 }
 
 module.exports = {
-  createAuthor,
-  validateAccount
+    createAuthor,
+    validateAccount
 }
