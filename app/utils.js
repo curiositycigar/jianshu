ed4dd2781334617d5c6e9ead6e6ff097507464d2
curiositycigar <you11098@163.com>
2018-03-25 23:19:54 +0800

    移除'返回结果预处理'方法

--- app/utils.js
+++ app/utils.js
@@ -4,33 +4,9 @@
 const crypto = require('crypto')
 
 module.exports = {
-  encrypt: function (data, salt, secret) {
-    return crypto.createHmac('sha256', secret || 'i love this secret!')
-      .update(data + (salt || 'salt' ))
-      .digest('hex');
-  },
-  resultHandler: function (result) {
-    if (result) {
-      if (result && result.name && result.errors && result.errors) {
-        //  错误信息
-        return {
-          error: {
-            message: result.errors.message,
-            code: -1
-          }
-        }
-      } else {
-        return {
-          data: {result}
-        }
-      }
-    } else {
-      return {
-        error: {
-          message: '未知错误',
-          code: -1
-        }
-      }
+    encrypt: function (data, salt, secret) {
+        return crypto.createHmac('sha256', secret || 'i love this secret!')
+            .update(data + (salt || 'salt'))
+            .digest('hex');
     }
-  }
 }
