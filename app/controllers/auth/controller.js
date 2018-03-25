39d25e408c0a084d0acd046244abe5ec5b12d85e
curiositycigar <you11098@163.com>
2018-03-25 23:12:10 +0800

    接口测试

--- app/controllers/auth/controller.js
+++ app/controllers/auth/controller.js
@@ -3,21 +3,22 @@
  */
 
 const {
-  createAuthor,
-  validateAccount
+    createAuthor,
+    validateAccount
 } = require('../../service/auth')
 
 exports.doRegister = async (ctx, next) => {
-
+    let result = await createAuthor({
+        email: 'test@mail.com',
+        password: '123456'
+    })
+    ctx.body = ctx.state.parseBody(result)
 }
 
 exports.doLogin = async (ctx, next) => {
-  ctx.body = await createAuthor({
-    email: 'test@mail.com',
-    password: '123456'
-  })
-}
-
-exports.doLogout = async (ctx, next) => {
-
+    let result = await validateAccount({
+        email: 'test@mail.com',
+        password: '123456'
+    })
+    ctx.body = ctx.state.parseBody(result, '用户名或密码错误')
 }
