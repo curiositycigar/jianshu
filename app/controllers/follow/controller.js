7d8745a8a3acb3eb03472ae592959b38498500a8
YOU <youwg@suninfo.com>
2018-04-24 17:39:19 +0800

    关注

--- app/controllers/follow/controller.js
+++ app/controllers/follow/controller.js
@@ -1,12 +1,153 @@
 /**
  * Created by YOU on 2018/4/23.
  */
-const router = require('koa-router')();
-const controller = require('./controller');
 const {
-  authenticated
-} = require('../../auth')
+  createFollowArticleGroup,
+  deleteFollowArticleGroup,
+  getFollowArticleGroups
+} = require('../../service/follow_article_group')
+const {
+  createFollowAuthor,
+  deleteFollowAuthor,
+  getFollowAuthors
+} = require('../../service/follow_author')
+const {
+  createFollowSubject,
+  deleteFollowSubject,
+  getFollowSubjects
+} = require('../../service/follow_subject')
+const {
+  getAuthorByIdList
+} = require('../../service/author')
+const {
+  getArticleGroupByIdList
+} = require('../../service/article_group')
+const {
+  geSubjectByIdList
+} = require('../../service/subject')
 
-router.get('/create', authenticated(), controller.createComment);
+exports.followAG = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['article_group_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await createFollowArticleGroup(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.followA = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['follow_author_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await createFollowArticleGroup(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.followS = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['subject_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await createFollowArticleGroup(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
 
-module.exports = router;
+exports.deleteAG = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['article_group_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await deleteFollowArticleGroup(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.deleteA = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['follow_author_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await deleteFollowAuthor(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.deleteS = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['subject_id']
+  let field = _.pick(params, requireField)
+  field.author_id = ctx.state[tokenKey].id
+  if (_.hasAll(field, requireField)) {
+    let result = await deleteFollowSubject(field)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.getFollowedAG = async (ctx, next) => {
+  let result = await getFollowArticleGroups({
+    author_id: ctx.state[tokenKey].id
+  })
+  let data = ctx.setBody(result)
+  ctx.body = data
+}
+exports.getFollowedA = async (ctx, next) => {
+  let result = await getFollowAuthors({
+    author_id: ctx.state[tokenKey].id
+  })
+  ctx.body = ctx.setBody(result)
+}
+exports.getFollowedS = async (ctx, next) => {
+  let result = await getFollowSubjects({
+    author_id: ctx.state[tokenKey].id
+  })
+  ctx.body = ctx.setBody(result)
+}
+exports.getAGFollowed = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['article_group_id']
+  let query = _.pick(params, requireField)
+  if (_.hasAll(query, requireField)) {
+    let result = await getFollowArticleGroups(query)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.getAFollowed = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['follow_author_id']
+  let query = _.pick(params, requireField)
+  if (_.hasAll(query, requireField)) {
+    let result = await getFollowAuthors(query)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
+exports.getSFollowed = async (ctx, next) => {
+  let params = ctx.query
+  let requireField = ['subject_id']
+  let query = _.pick(params, requireField)
+  if (_.hasAll(query, requireField)) {
+    let result = await getFollowSubjects(query)
+    ctx.body = ctx.setBody(result)
+  } else {
+    ctx.throw(400)
+  }
+}
