d705134325462fae2fd2aac34024d62400f26a5d
YOU <youwg@suninfo.com>
2018-04-25 17:34:10 +0800

    main-router更改

--- app/routers.js
+++ app/routers.js
@@ -8,6 +8,8 @@ const article = require('./controllers/article')
 const articleGroup = require('./controllers/article_group')
 const Subject = require('./controllers/subject')
 const Contribute = require('./controllers/contribute')
+const Follow = require('./controllers/follow')
+const Comment = require('./controllers/comment')
 
 module.exports = function () {
   router.use('/author', author.routes(), author.allowedMethods())
@@ -15,5 +17,7 @@ module.exports = function () {
   router.use('/article-group', articleGroup.routes(), articleGroup.allowedMethods())
   router.use('/subject', Subject.routes(), Subject.allowedMethods())
   router.use('/contribute', Contribute.routes(), Contribute.allowedMethods())
+  router.use('/comment', Comment.routes(), Comment.allowedMethods())
+  router.use('/follow', Follow.routes(), Follow.allowedMethods())
   return router.routes()
 }
